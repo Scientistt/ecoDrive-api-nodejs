9faeeabfec362c059dbc135d13ac76d8156f5580
Scientistt <fabio.vitor.noronha@gmail.com>
2025-04-29 23:56:45 -0400

    feat: Bucket operations

--- src/buckets/service/bucket.service.js
+++ src/buckets/service/bucket.service.js
@@ -1,5 +1,13 @@
+const awsService = require('../../utils/aws.service')
+
 module.exports = {
-    async listBucketsFromAWS() {
-        return "Listou TUDOOOO!!!!!";
+    async listBuckets() {
+        let myBuckets = await awsService.listBuckets();
+        return myBuckets;
+    },
+
+    async getBucketInfo(bucketName) {
+        let myBuckets = await awsService.getBucketInfo(bucketName);
+        return myBuckets;
     },
 };
